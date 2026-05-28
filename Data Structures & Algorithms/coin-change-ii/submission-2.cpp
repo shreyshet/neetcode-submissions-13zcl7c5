65219cd1da4c4845fdc112f039b0918a49526e7f
Shreyansh Shethia <shreyanshjain987@gmail.com>
2026-05-27 20:51:44 -0400

    Add: coin-change-ii - submission-2

---
+++ Data Structures & Algorithms/coin-change-ii/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countchange(int target, int i, vector<int>& coins,vector<vector<int>>& store){
+        if (i >= coins.size()) return 0;
+        if(target < 0) return 0;
+        if(target <= 0) return 1;
+        if (store[i][target] != -1) return store[i][target]; 
+        return store[i][target] = countchange(target - coins[i], i, coins, store) + countchange(target, i+1, coins, store);
+    }
+
+    int change(int amount, vector<int>& coins) {
+        if (amount == 0) return 1;
+        vector<vector<int>> store(coins.size(),vector<int>(amount+1,-1));
+        int ret = 0;
+        ret = countchange(amount,0,coins,store);
+        return ret;
+    }
+};
